c826183b5d78c588f83988a414fa2c952ed352fa
熊井利充 <the.brown.bunnyy@gmail.com>
2020-05-05 20:58:05 +0900

    画面の実装

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,73 @@
 <template>
-  <div>
+<div>
     <nuxt />
-  </div>
+</div>
 </template>
 
 <style>
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+    font-family: 'sans-serif', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+        Roboto, 'Helvetica Neue', Arial, sans-serif;
+    font-size: 16px;
+    word-spacing: 1px;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    box-sizing: border-box;
+    background-color: rgb(33, 32, 32);
+    color: #B3C0D1;
 }
 
 *,
 *:before,
 *:after {
-  box-sizing: border-box;
-  margin: 0;
+    box-sizing: border-box;
+    margin: 0;
 }
 
 .button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+    display: inline-block;
+    border-radius: 4px;
+    border: 1px solid #3b8070;
+    color: #3b8070;
+    text-decoration: none;
+    padding: 10px 30px;
 }
 
 .button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+    color: #fff;
+    background-color: #3b8070;
 }
 
 .button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
+    display: inline-block;
+    border-radius: 4px;
+    border: 1px solid #35495e;
+    color: #35495e;
+    text-decoration: none;
+    padding: 10px 30px;
+    margin-left: 15px;
 }
 
 .button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+    color: #fff;
+    background-color: #35495e;
 }
+
+.el-header {
+    background-color: #333;
+    line-height: 50px;
+    font-size: 24px;
+}
+
+.el-row {
+    margin-bottom: 20px;
+}
+
+.el-card {
+    background-color: dimgray;
+    color: #B3C0D1;
+}
+
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,64 +1,143 @@
 <template>
-<div class="container">
-    <div>
-        <logo />
-        <h1 class="title">
-            RTCChat
-        </h1>
-        <h2 class="subtitle">
-            My gnarly Nuxt.js project
-        </h2>
-        <div class="links">
-            <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-                Documentation
-            </a>
-            <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">
-                GitHub
-            </a>
-        </div>
-    </div>
-</div>
+<el-container style="height: 100vh">
+    <el-header style="font-family:Pacifico">Our Chaaaaaat!!</el-header>
+    <el-container>
+        <el-aside width="250px">
+          <p>Members</p>
+          <ul v-if="room" style="list-style: none">
+            <li>{{my_peer_id}} (you)</li>
+            <li v-for="(member,index) in room.members" :key="index">{{member}}</li>
+          </ul>
+        </el-aside>
+        <el-main v-if="room">
+            <el-row>
+                <el-col :span="24">
+                    <div class="block">
+                        <el-timeline style="overflow:auto; height:60vh">
+                            <el-timeline-item v-for="(message ,index) in messages" :key="index" :id="message.id" class="infinite-list-item" :timestamp="message.time" placement="top">
+                                <el-card>
+                                    <h4>{{message.peer_id}}</h4>
+                                    <p>{{message.message}}</p>
+                                </el-card>
+                            </el-timeline-item>
+                        </el-timeline>
+                    </div>
+                </el-col>
+            </el-row>
+            <el-row>
+                <el-col :span="23" :offset="1">
+                    <el-input type="textarea" :autosize="{ minRows: 2, maxRows: 4}" placeholder="Please input" v-model="local_message" />
+                </el-col>
+            </el-row>
+            <el-row>
+                <el-col :span="23" :offset="1">
+                    <el-button @click="sendMessage">send</el-button>
+                </el-col>
+            </el-row>
+        </el-main>
+        <el-main v-else>
+            <el-row>
+                <el-col :span="10">
+                    <p>Join Room</p>
+                </el-col>
+            </el-row>
+            <el-row>
+                <el-col :span="2">
+                    room name
+                </el-col>
+                <el-col :span="10">
+                    <el-input type="text" v-model="room_id" />
+                </el-col>
+            </el-row>
+            <el-row>
+                <el-col :span="10" :offset="2">
+                    <el-button @click="makeRoom">join</el-button>
+                </el-col>
+            </el-row>
+        </el-main>
+    </el-container>
+</el-container>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-    components: {
-        Logo
-    }
+    data() {
+        return {
+            my_peer_id: '',
+            name: '',
+            room_id: 'test',
+            room: null,
+            messages: [],
+            local_message: '',
+        }
+    },
+    mounted: function () {
+        const Peer = require("skyway-js");
+
+        this.peer = new Peer({
+            key: process.env.API_KEY,
+            debug: 3,
+        });
+
+        this.peer.on('open', () => {
+            this.my_peer_id = this.peer.id;
+        });
+
+    },
+    methods: {
+        makeRoom: function () {
+            const room = this.peer.joinRoom(this.room_id, {
+                mode: 'sfu',
+            });
+
+            this.room = room;
+
+            room.once('open', () => {
+                this.pushMessage(this.peerId, 'You joined');
+            });
+
+            room.once('close', () => {
+                this.pushMessage(this.peerId, 'You lefted');
+            });
+
+            room.on('peerJoin', peerId => {
+                this.pushMessage(peerId, `${peerId} joined`);
+            });
+
+            room.on('peerLeave', peerId => {
+                this.pushMessage(peerId, `${peerId} lefted`);
+            });
+
+            room.on('data', ({
+                data,
+                src
+            }) => {
+                this.pushMessage(src, data);
+            });
+        },
+        sendMessage: function () {
+            this.room.send(this.local_message);
+            this.pushMessage(this.my_peer_id, this.local_message);
+            this.local_message = '';
+        },
+        pushMessage: function (peer_id, message) {
+            const id = `message-${this.messages.length}`;
+            this.messages.push({
+                id: id,
+                peer_id: peer_id,
+                time: this.now(),
+                message: message,
+            });
+            this.$nextTick(() => {
+                document.getElementById(id).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        },
+        now: function () {
+            let dd = new Date();
+            return `${dd.getHours()}:${dd.getMinutes()}:${dd.getSeconds()}`;
+        }
+    },
 }
 </script>
-
-<style>
-.container {
-    margin: 0 auto;
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-}
-
-.title {
-    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-        'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-    display: block;
-    font-weight: 300;
-    font-size: 100px;
-    color: #35495e;
-    letter-spacing: 1px;
-}
-
-.subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-}
-
-.links {
-    padding-top: 15px;
-}
-</style>
